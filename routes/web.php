727c83133c0efb285355f5887e9c074e0c34a2c8
Uchenna <uchennapaschal50@gmail.com>
2021-01-18 15:15:32 +0100

    redirect home page to entries page

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\EntryController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->to('/entries');
 });
 
 Route::resource('departments', departmentController::class);
