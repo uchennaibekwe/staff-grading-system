5c3c16f7fa14825a5147f3b2f71a90263e072177
Uchenna <uchennapaschal50@gmail.com>
2021-01-18 15:01:27 +0100

    resolve datatable issue

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -103,7 +103,7 @@
             <!-- /#page-wrapper -->
         </div>
         <!-- /#wrapper -->
-
+        
         <!-- jQuery -->
         <script src="../js/jquery.min.js"></script>
 
@@ -113,7 +113,19 @@
         <!-- Metis Menu Plugin JavaScript -->
         <script src="../js/metisMenu.min.js"></script>
 
+        <!-- DataTables JavaScript -->
+        <script src="../js/dataTables/jquery.dataTables.min.js"></script>
+        <script src="../js/dataTables/dataTables.bootstrap.min.js"></script>
+
         <!-- Custom Theme JavaScript -->
         <script src="../js/startmin.js"></script>
+
+        <script>
+            $(document).ready(function() {
+                $('#dataTables-example').DataTable({
+                        responsive: true
+                });
+            });
+        </script>
     </body>
 </html>
